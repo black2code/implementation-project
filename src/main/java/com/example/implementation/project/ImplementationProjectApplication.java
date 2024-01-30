2546c8346ef3ff5fa3e45e3d0d4ec29721fb5a28
black2code <alsdnr1080@gmail.com>
2024-01-31 01:43:15 +0900

    first commit

---
+++ src/main/java/com/example/implementation/project/ImplementationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.implementation.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/implementation/project/ImplementationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.implementation.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
